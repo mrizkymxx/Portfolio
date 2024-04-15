69e0c60e44fa0b84fde8857924e61aa37a5a0278
mrizkymxx <159142542+mrizkymxx@users.noreply.github.com>
2024-04-15 11:06:59 +0000

    mengganti form email

--- forms/contact.php
+++ forms/contact.php
@@ -1,7 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = 'muhamrizky00@gmail.com';
-
     $subject = $_POST['subject'];
     $name = $_POST['name'];
     $from = $_POST['email'];
@@ -12,8 +11,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                   "Email: $from\n\n" .
                   "Message:\n$message";
 
+    // Headers tambahan
+    $headers = "From: $name <$from>" . "\r\n" .
+               "Reply-To: $from" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
     // Mengirim email
-    if (mail($to, $subject, $email_body)) {
+    if (mail($to, $subject, $email_body, $headers)) {
         echo "Your message has been sent. Thank you!";
     } else {
         echo "Failed to send message. Please try again.";
